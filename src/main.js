0835f46f86c16b8b90d288fa668a046f2f447f7d
Caritopli5 <carolina.medina@usach.cl>
2019-07-05 11:42:37 -0400

    cambio en html y css

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 /* Manejo del DOM */
+const btnType = document.getElementById("type");
+
+btnType.addEventListener("click", ()=> {
+	    // sobresale el boton
+	    // mostrar menu desplegable
+
+})
