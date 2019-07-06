53535d9623107fdb6131cd741c08cf99755cc71e
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-07-06 01:18:28 -0400

    HTML <section><nav id=dropdownMenu> para DOM; main.js tiene HTML Dinamico que muestra menu desplegable; data.js tiene funcion amountOfPokesByType()

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 /* Manejo del DOM */
 const btnType = document.getElementById("type");
 
-btnType.addEventListener("click", ()=> {
-	    // sobresale el boton
-	    // mostrar menu desplegable
-
-})
+btnType.addEventListener("click", ()=> { // mostrar menu desplegable
+	dropdownMenu.innerHTML = `<select id="dropdownType"></select>`//creo menú desplegable
+	let ArrayType = arrayPokeType(); //arreglo de tipos
+	for (let i=0; i< ArrayType.length; i++) { // recorro el arreglo de tipos para escribir en 'dropdownType' los <option> con tipos pokemon
+		let node = document.createElement("OPTION");
+		let textNode = document.createTextNode(ArrayType[i]);
+		node.appendChild(textNode);
+		document.getElementById("dropdownType").appendChild(node);
+		document.getElementsByTagName("OPTION")[i].setAttribute("id", ArrayType[i]); //Asigna id a cada <option> igual al tipo que alberga
+	}
+});
