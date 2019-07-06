45264be18c06e89678839d9fb2638ae7f27a032d
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-07-06 12:14:27 -0400

    funcion de prueba en DOM para ver si pasa algo cuando seleccionamos algo en menú desplegable

--- src/main.js
+++ src/main.js
@@ -2,13 +2,36 @@
 const btnType = document.getElementById("type");
 
 btnType.addEventListener("click", ()=> { // mostrar menu desplegable
-	dropdownMenu.innerHTML = `<select id="dropdownType"></select>`//creo menú desplegable
+	dropdownMenu.innerHTML = `<select id="dropdownType"></select>
+														<div id="pokes"></div>`//creo menú desplegable
 	let ArrayType = arrayPokeType(); //arreglo de tipos
 	for (let i=0; i< ArrayType.length; i++) { // recorro el arreglo de tipos para escribir en 'dropdownType' los <option> con tipos pokemon
-		let node = document.createElement("OPTION");
-		let textNode = document.createTextNode(ArrayType[i]);
-		node.appendChild(textNode);
-		document.getElementById("dropdownType").appendChild(node);
-		document.getElementsByTagName("OPTION")[i].setAttribute("id", ArrayType[i]); //Asigna id a cada <option> igual al tipo que alberga
+		let node = document.createElement("OPTION");// creo <option></option>
+		let textNode = document.createTextNode(ArrayType[i]);// creo lo que quiero poner dentro de <option></option>
+		node.appendChild(textNode);//lo pongo dentro
+		document.getElementById("dropdownType").appendChild(node);//en <select id="dropdownType"></select> agrego el <option></option> creado
+		document.getElementsByTagName("OPTION")[i].setAttribute("value", ArrayType[i]); //Asigna value a cada <option> igual al tipo que alberga
 	}
+	$("#dropdownType").change(function(){//que pase algo cuando elegimos una opcion del menú desplegable
+	  alert("Esto lo cambiamos por lo que queremos que pase cuando seleccionemos un tipo.");
+	});
+
 });
+
+
+
+/*
+let dropdownMenuOption = (stringType) => {
+	let optionType = document.getElementById(stringType);
+	optionType.addEventListener("change", () => {
+		let typeArray = arrayDataType(stringType);
+		for (let i=0; i< typeArray.length; i++) {
+			let node = document.createElement("DIV");
+			let textNode = document.createTextNode(typeArray[i].name);
+			node.appendChild(textNode);
+			document.getElementById("pokes").appendChild(node);
+			document.getElementsByTagName("DIV")[i].setAttribute("id", typeArray[i].name);
+		}
+	}
+)};
+*/
