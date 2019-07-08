3340af13649e03594ad49ebcb2cffac44197f5f8
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-07-08 09:34:27 -0400

    muestra pokemones cuando eliges un tipo [adaptado a los cambios de Colaboradora]

--- src/main.js
+++ src/main.js
@@ -1,23 +1,10 @@
 /* Manejo del DOM */
-const btnType = document.getElementById("type");
+const btnType = document.getElementById("select_type");
 
-btnType.addEventListener("click", ()=> { // mostrar menu desplegable
-	dropdownMenu.innerHTML = `<select id="dropdownType"></select>
-														<div id="pokes"></div>`//creo menú desplegable
-	let ArrayType = arrayPokeType(); //arreglo de tipos
-	for (let i=0; i< ArrayType.length; i++) { // recorro el arreglo de tipos para escribir en 'dropdownType' los <option> con tipos pokemon
-		let node = document.createElement("OPTION");// creo <option></option>
-		let textNode = document.createTextNode(ArrayType[i]);// creo lo que quiero poner dentro de <option></option>
-		node.appendChild(textNode);//lo pongo dentro
-		document.getElementById("dropdownType").appendChild(node);//en <select id="dropdownType"></select> agrego el <option></option> creado
-		node.setAttribute("value", ArrayType[i]);
-		//document.getElementsByTagName("OPTION")[i].setAttribute("value", ArrayType[i]); //Asigna value a cada <option> igual al tipo que alberga
-	}
-	$("#dropdownType").change( ()=> {//que pase algo cuando elegimos una opcion del menú desplegable
-	  dropdownMenuOption(document.getElementById("dropdownType").value);//toma el valor elegido en el menú desplegable. Ej.: "Fire"
-
+btnType.addEventListener("click", ()=> {
+	$("#select_type").change( ()=> {//que pase algo cuando elegimos una opcion del menú desplegable
+	  dropdownMenuOption(document.getElementById("select_type").value);//toma el valor elegido en el menú desplegable. Ej.: "Fire"
 	});
-
 });
 
 let dropdownMenuOption = (stringType) => {
