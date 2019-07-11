d2c902be6a11257e2ba6d15a1b1f23aa719208f9
Caritopli5 <carolina.medina@usach.cl>
2019-07-11 13:03:48 -0400

    test resuelto

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 const btnType = document.getElementById("type");
 //var amountOfPokesByType = 0;
 //var POKEMON = [];
-window.POKEMON = POKEMON;
+//window.POKEMON = POKEMON;
 //var pokeData;
 
 
@@ -12,7 +12,7 @@ btnType.addEventListener("click", ()=> {
 	document.getElementById("select_type").addEventListener("change", ()=> {//que pase algo cuando elegimos una opcion del menú desplegable
 		showPercentage(document.getElementById("select_type").value);//Agrego información al article id="info"
 		document.getElementById("info").style.display = "block";
-
+		document.getElementById("info").style.color= "#ff8000";
 		dropdownMenuOption(document.getElementById("select_type").value);//toma el valor elegido en el menú desplegable. Ej.: "Fire"
 	});	
 
@@ -20,21 +20,21 @@ btnType.addEventListener("click", ()=> {
 
 let dropdownMenuOption = (stringType) => {//RECONOCE EL TIPO ELEGIDO POR EL USUARIO Y ESCRIBE LOS NOMBRES DE POKEMONES DE ESE TIPO
 	//var arrayDataType;
-	let arrayPoke = arrayDataType(stringType);//arreglo de pokes del tipo stringType
-	document.getElementById("pokes").innerHTML="";//limpiar div en caso de que tenga pokes
-	for (let j=0; j<arrayPoke.length; j++) { //recorro el arreglo de pokes
+		let arrayPoke = window.pokeObject.arrayDataType(stringType);//arreglo de pokes del tipo stringType
+		document.getElementById("pokes").innerHTML="";//limpiar div en caso de que tenga pokes
+		for (let j=0; j<arrayPoke.length; j++) { //recorro el arreglo de pokes
 		let node = document.createElement("DIV");//creo el contenedor <div></div>
 
 		//let node2 = document.createElement("DIV");
 		//let textNode = document.createTextNode("Hola, soy "+arrayPoke[j].name + ". Los pokemon tipo "+ pokeWeaknesses(arrayPoke[j].name) + " me hacen mucho daño. Aparezco a las "+ arrayPoke[j].spawn_time);//escribo el nombre del poke en la posición j
 		//node.style.color = "red";
     var img=document.createElement("img");
-	    img.src=arrayPoke[j].img;
-	    img.id="picture"
-	    node.appendChild(img);
+		img.src=arrayPoke[j].img;
+		img.id="picture"
+		node.appendChild(img);
 		let phrase = document.createElement("P");
 			phrase.id="phrase"+arrayPoke[j].name;
-		let textPhrase = document.createTextNode("Hola, soy "+arrayPoke[j].name + ". Los pokemon tipo "+ pokeWeaknesses(arrayPoke[j].name) + " me hacen mucho daño. Aparezco a las "+ arrayPoke[j].spawn_time);//escribo el nombre del poke
+		let textPhrase = document.createTextNode("Hola, soy "+arrayPoke[j].name + ". Los pokemon tipo "+ window.pokeObject.pokeWeaknesses(arrayPoke[j].name) + " me hacen mucho daño. Aparezco a las "+ arrayPoke[j].spawn_time);//escribo el nombre del poke
 		phrase.appendChild(textPhrase);
 		node.appendChild(phrase);
 		node.appendChild(document.createElement("br"));
@@ -79,6 +79,7 @@ let dropdownMenuOption = (stringType) => {//RECONOCE EL TIPO ELEGIDO POR EL USUA
 		document.getElementById(arrayPoke[j].name).addEventListener("mouseout", ()=> {//que pase algo cuando sacamos el mouse de la imagen
 			document.getElementById("phrase"+arrayPoke[j].name).style.display = "none";
 		});
+		document.getElementById("phrase" + arrayPoke[j].name).style.color= "#ff8000";
 	}
 }
 
@@ -88,8 +89,8 @@ let showPercentage = (stringType) => {//MOSTRAR PORCENTAJE DE TIPOS POKEMON
 	document.getElementById("info").innerHTML="";//limpiar article en caso de que tenga info
 	let node = document.createElement("DIV");//creo el contenedor <div></div>
 	let textNode = document.createTextNode("El porcentaje de los pokémon tipo "+stringType+ " es " + 
-		pokePercentage(stringType).toFixed(2) + "%. (" + amountOfPokesByType(stringType) + " de " + 
-		pokeData.length + ").");
+		window.pokeObject.pokePercentage(stringType).toFixed(2) + "%. (" + window.pokeObject.amountOfPokesByType(stringType) + " de " + 
+		window.pokeData.length + ").");
 	node.appendChild(textNode);//agrego el porcentaje al contenedor <div></div>
 	document.getElementById("info").appendChild(node);//agrego el nodo al article con id="info"
 }
